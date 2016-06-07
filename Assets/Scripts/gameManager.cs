632a4d88e4af894d050811b7984b2a462df373a4
Gyanreyer <gyanreyer@gmail.com>
2016-06-07 00:12:43 -0400

    Final Version
    Cleaned up scripts and made sure everything is well documented

--- Assets/Scripts/gameManager.cs
+++ Assets/Scripts/gameManager.cs
@@ -2,19 +2,21 @@
 using System.Collections;
 
 
-//Struct holds ints for x and y coords of lights that have been selected
-public struct lightCoords
+//Struct holds ints for x and y coords of lights
+public struct gridCoords
 {
+    //x and y coordinates on grid
     public int x, y;
 
-    public lightCoords(int xPos, int yPos)
+    //Constructor takes x and y coordinates and stores them
+    public gridCoords(int xPos, int yPos)
     {
         x = xPos;
         y = yPos;
     }
 }
 
-
+//GameManager class handles spawning and managing grid of lights
 public class gameManager : MonoBehaviour {
 
     //Prefab for lights
@@ -27,20 +29,21 @@ public class gameManager : MonoBehaviour {
     public int gridWidth = 10;
     public int gridHeight = 10;
 
-
     //Selection stuff for lights
-    private lightCoords startSelection;
+    private gridCoords startSelection;//Coordinates of first light bulb clicked, when a second is clicked lights will change state within the rectangle formed by the two coordinates
 
-    private bool selectionStarted;
+    private bool firstSelected;//Whether the user has selected the first light
 
-    //Property to return whether selection started
-    public bool SelectionStarted { get { return selectionStarted; } }
+    //Property to return whether first selected
+    public bool FirstSelected { get { return firstSelected; } }
 
 
 	// Use this for initialization
-	void Start () {
-
+	void Start ()
+    {
+        //Get child of light prefab because it contains the sprites and visual aspects, the parent object exists so that position animations can be played that won't mess up the child
         GameObject lightChild = lightPrefab.transform.GetChild(0).gameObject;
+
 
         //Get dimensions of orthographic camera in terms of world units in order to scale lights so they fit in the view
         float worldScreenHeight = Camera.main.orthographicSize * 2f;//orthographicSize = half of camera height
@@ -50,26 +53,28 @@ public class gameManager : MonoBehaviour {
 
         Vector3 lightBounds = lightChild.GetComponent<SpriteRenderer>().bounds.size;//Bounds in world units for light
 
-        //The lights are going to be square so we'll only adjust the height and then use the same value for width
-        float adjustedLightHeight = worldScreenSize.y / (lightBounds.y * gridHeight);//Divide world height of screen by world height of light to get local height                                                                                                                       
 
-        lightChild.transform.localScale = new Vector3(adjustedLightHeight, adjustedLightHeight, 1);//Set light prefab scale to adjusted value
+        //The light grid is going to be square so we'll only adjust to fit within the screen height and use it for both width and height
+        float adjustedLightDimension = worldScreenSize.y / (lightBounds.y * gridHeight);//Divide world height of screen by world height of light to get local height
+                                                                                        //Also divide by height of grid so that size is proportioned to size of one spot on grid
+                                                                                                                     
+        lightChild.transform.localScale = new Vector3(adjustedLightDimension, adjustedLightDimension, 1);//Set light scale to adjusted value
 
         lightBounds = lightChild.GetComponent<SpriteRenderer>().bounds.size;//Update bounds for new adjusted size, we'll use this for spawn locations
 
         //Initialize light grid to specified width and height
         lightGrid = new GameObject[gridWidth, gridHeight];
 
-        Vector3 spawnStartPos = new Vector3(-worldScreenHeight/2, -worldScreenHeight/2,0);
+        Vector3 spawnStartPos = new Vector3(-worldScreenHeight/2, -worldScreenHeight/2,0);//Start position for spawning
 
         //Loop through all elements of lightMatrix and spawn a light prefab
         for(int y = 0; y < gridHeight; y++)
         {
             for (int x = 0; x < gridWidth; x++)
             {
-                lightPrefab.transform.GetChild(0).position = spawnStartPos + new Vector3(x * lightBounds.x, y * lightBounds.y, 0);
+                lightPrefab.transform.GetChild(0).position = spawnStartPos + new Vector3(x * lightBounds.x, y * lightBounds.y, 0);//Set position for child before instantiating
                 lightGrid[x, y] = (GameObject)Instantiate(lightPrefab);
-                lightGrid[x, y].transform.GetChild(0).GetComponent<lightScript>().coords = new lightCoords(x,y);
+                lightGrid[x, y].transform.GetChild(0).GetComponent<lightScript>().coords = new gridCoords(x,y);//Set grid coordinates for new light so that it can keep track of itself in the light grid
             }
         }
 	}
@@ -84,19 +89,19 @@ public class gameManager : MonoBehaviour {
     }
 
 
-    //Update lights if one is selected or hovered over during selection
-    public void LightUpdate(lightCoords coords, bool selected)//selected is true if the light was clicked on, false if just hovered
+    //Update lights if one is selected or hovered over
+    public void LightUpdate(gridCoords coords, bool clicked)//clicked is true if the light was clicked on, false if just hovered
     {
         //If selection of lights has not been started yet, store coords as initial selection and set selection state to started
-        if(!selectionStarted)
+        if(!firstSelected)
         {
             startSelection = coords;
-            selectionStarted = true;
+            firstSelected = true;
         }
         //Otherwise, highlight or switch appropriate lights
         else
         {
-            selectionStarted = !selected;//If second selection was made, end selection state, otherwise this will stay true
+            firstSelected = !clicked;//If second selection was made, reset selected state, otherwise this will stay true
 
             //Direction that x and y will need to be incremented by in loop, will be -1, 0, or 1 depending on where end is in relation to start
             int yDirection = coords.y - startSelection.y;
@@ -111,7 +116,7 @@ public class gameManager : MonoBehaviour {
             int y = startSelection.y;
             int x;
 
-            GameObject currentLight;
+            GameObject currentLight;//Will represent the current object being worked with each loop
 
             //Using do while so that in instances where the x or y of start and end are equal it'll still loop at least once
             //Outer loop goes through y coords
@@ -119,17 +124,15 @@ public class gameManager : MonoBehaviour {
             {
                 x = startSelection.x;//Reset x counter
 
-                //Inner loop through x coords
+                //Inner loop goes through x coords
                 do
                 {
-                    currentLight = lightGrid[x, y].transform.GetChild(0).gameObject;
+                    currentLight = lightGrid[x, y].transform.GetChild(0).gameObject;//Set current light to GO at current x and y
 
-                    //Switch current light
-                    if (selected)
-                    {
+                    //If user clicked, switch current light's state
+                    if (clicked)
                         currentLight.GetComponent<lightScript>().switchLight();
-                        currentLight.GetComponent<lightScript>().Highlighted = false;
-                    }
+                    //Otherwise, just highlight this light
                     else
                         currentLight.GetComponent<lightScript>().Highlighted = true;
 
--- Assets/Scripts/lightScript.cs
+++ Assets/Scripts/lightScript.cs
@@ -6,16 +6,16 @@ public class lightScript : MonoBehaviour {
     //Enum for state of light, whether off, half-lit, or fully on
     enum lightState
     {
-        off = 0,
-        half = 1,
-        on = 2
+        off,
+        half,
+        on
     }
 
     //Sprite renderer for this light
     private SpriteRenderer sprRenderer;
 
-    //Parent object which is used for animation
-    private GameObject parentObj;
+    //Animator on parent object used for animations
+    private Animator parentAnimator;
 
     //Game manager
     private gameManager GM;
@@ -25,12 +25,13 @@ public class lightScript : MonoBehaviour {
     public Sprite halfSprite;
     public Sprite onSprite;
 
-    public lightCoords coords;//Coordinates of this light on grid    
+    public gridCoords coords;//Coordinates of this light on grid    
 
     private lightState state;//State that light is in
 
     private bool highlighted;//True when bulb is highlighted
 
+    //Property for getting/setting highlighted
     public bool Highlighted
     {
         get { return highlighted; }
@@ -42,28 +43,30 @@ public class lightScript : MonoBehaviour {
         state = lightState.off;
 
         sprRenderer = GetComponent<SpriteRenderer>();
+        parentAnimator = transform.parent.gameObject.GetComponent<Animator>();
 
-        GM = GameObject.FindGameObjectWithTag("GameManager").GetComponent<gameManager>();
+        GM = GameObject.FindGameObjectWithTag("GameManager").GetComponent<gameManager>();        
 	}
 	
 	// Update is called once per frame
 	void Update () {
         //Set color to reflect whether highlighted or not
         if (highlighted)     
-            sprRenderer.color = Color.white;
+            sprRenderer.color = Color.white;//When highlighted color lights it up brighter
         else
-            sprRenderer.color = new Color(0.85f, 0.85f, 0.85f);
+            sprRenderer.color = new Color(0.85f, 0.85f, 0.85f);//When not highlighted color dims sprite
 
-        transform.parent.gameObject.GetComponent<Animator>().SetBool("Highlighted", highlighted);//Play pop animation if highlighted
+        parentAnimator.SetBool("Highlighted", highlighted);//Play pop animation if highlighted, will switch state from Idle to PopLight and then back to Idle if highlighed is false again
 
-        highlighted = false;//Reset highlighted so light won't be left highlighted when shouldn't
+        highlighted = false;//Reset highlighted so light won't be left highlighted when it shouldn't, if it should it'll get set back to true next frame
 	}
 
     //Switch state of light
     public void switchLight()
     {
-        //Cycle through states using index values of lightState enum
+        //Cycle through states using int values of lightState enum, much more compact than having to deal with switch or if statements
         state = (lightState)(((int)state + 1) % 3);
+
 
         //Change sprite based on current state
         switch(state)
@@ -83,23 +86,23 @@ public class lightScript : MonoBehaviour {
 
     }
 
-    //If user clicks on this lightbulb's collider, call LightSelected in game manager to either set start point or switch the appropriate row of lights
+    //If user clicks on this lightbulb's collider, call LightUpdate with clicked being true in game manager to either set start point or switch the appropriate lights
     void OnMouseDown()
     {
         GM.LightUpdate(coords, true);
-        
-             
     }
 
-    //If user mouses over this lightbulb's collider, alter color slightly and call LightHovered to highlight all possible lightbulbs in selection
+    //If user mouses over this lightbulb's collider
     void OnMouseOver()
     {
-        if (GM.SelectionStarted)
-            GM.LightUpdate(coords, false);
+        //If first has been selected, update lights to highlight the rectangle between start point and this light to give a visual representation of what will change if user clicks
+        if (GM.FirstSelected)
+            GM.LightUpdate(coords, false);//Call LightUpdate with clicked being false since only hovering right now
         else
-            highlighted = true;
+            highlighted = true;//If first hasn't been selected, just highlight this one
     }
 
+    //If user's mouse leaves this light's collider, set highlighted back to false
     void OnMouseExit()
     {
         highlighted = false;
